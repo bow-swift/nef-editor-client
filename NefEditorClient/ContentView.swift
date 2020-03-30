205eee2c15fd41252d25411472968b80a83f3442
Tomás <truizlop@gmail.com>
2020-03-30 14:38:46 +0200

    Initial Commit

---
+++ NefEditorClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NefEditorClient
+//
+//  Created by Tomás Ruiz López on 30/03/2020.
+//  Copyright © 2020 The Bow Authors. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
