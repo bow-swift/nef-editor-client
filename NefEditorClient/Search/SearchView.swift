e878838f712bacd76f06bb84c38e27c60404b68b
Tomás <truizlop@gmail.com>
2020-04-06 13:50:00 +0200

    Initial layout for SearchView

--- NefEditorClient/Search/SearchView.swift
+++ NefEditorClient/Search/SearchView.swift
@@ -1,10 +1,33 @@
 import SwiftUI
+import GitHub
 
 struct SearchView: View {
+    @EnvironmentObject var orientationInfo: OrientationInfo
+    let repositories: Repositories
+    @State var query: String = ""
+    
     var body: some View {
-        ZStack {
-            Color.gray
-            Text("Search")
+        VStack {
+            SearchBar(placeholder: "Search repositories...", query: $query)
+            
+            ScrollView {
+                RepositoryGridView(repositories: repositories, columns: self.columns)
+                .padding()
+            }
         }
+    }
+    
+    var columns: Int {
+        switch orientationInfo.orientation {
+        case .landscape: return 2
+        case .portrait: return 1
+        }
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView(repositories: sampleSearchResults)
+        .environmentObject(OrientationInfo())
     }
 }
