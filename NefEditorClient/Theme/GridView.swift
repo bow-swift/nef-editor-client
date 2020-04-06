08eab89d2f96eb4589a2a79b3969e84be6a37fd8
Tomás <truizlop@gmail.com>
2020-04-06 10:31:23 +0200

    Add customization options to GridView

--- NefEditorClient/Theme/GridView.swift
+++ NefEditorClient/Theme/GridView.swift
@@ -3,18 +3,33 @@ import SwiftUI
 struct GridView<Cell: View>: View {
     let rows: Int
     let columns: Int
+    let horizontalSpacing: CGFloat
+    let verticalSpacing: CGFloat
+    let horizontalAlignment: HorizontalAlignment
+    let verticalAlignment: VerticalAlignment
     let cellAt: (Int, Int) -> Cell
     
-    init(rows: Int, columns: Int, @ViewBuilder cellAt: @escaping (Int, Int) -> Cell) {
+    init(
+        rows: Int,
+        columns: Int,
+        horizontalSpacing: CGFloat = 8,
+        verticalSpacing: CGFloat = 8,
+        horizontalAlignment: HorizontalAlignment = .center,
+        verticalAlignment: VerticalAlignment = .center,
+        @ViewBuilder cellAt: @escaping (Int, Int) -> Cell) {
         self.rows = rows
         self.columns = columns
+        self.horizontalSpacing = horizontalSpacing
+        self.verticalSpacing = verticalSpacing
+        self.horizontalAlignment = horizontalAlignment
+        self.verticalAlignment = verticalAlignment
         self.cellAt = cellAt
     }
     
     var body: some View {
-        VStack {
+        VStack(alignment: self.horizontalAlignment, spacing: self.horizontalSpacing) {
             ForEach(0 ..< self.rows) { row in
-                HStack {
+                HStack(alignment: self.verticalAlignment, spacing: self.verticalSpacing) {
                     ForEach(0 ..< self.columns) { column in
                         self.cellAt(row, column)
                     }
@@ -26,10 +41,21 @@ struct GridView<Cell: View>: View {
 
 struct GridView_Previews: PreviewProvider {
     static var previews: some View {
-        GridView(rows: 4, columns: 3) { row, column in
-            ZStack {
-                Circle().fill(Color.gray)
-                Text("\(row), \(column)")
+        Group {
+            GridView(rows: 4, columns: 3) { row, column in
+                ZStack {
+                    Circle().fill(Color.gray)
+                    Text("\(row), \(column)")
+                }
+            }
+            
+            ScrollView {
+                GridView(rows: 20, columns: 6, horizontalSpacing: 24, verticalSpacing: 16) { row, column in
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 8).fill(Color.blue)
+                        Text("\(row), \(column)")
+                    }.aspectRatio(4/3, contentMode: .fit)
+                }
             }
         }
     }
