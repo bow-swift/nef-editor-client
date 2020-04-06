2354291059d24c2ffad3eb176856a94f8a3e6d9a
Tomás <truizlop@gmail.com>
2020-04-06 14:16:27 +0200

    Add loading search view

---
+++ NefEditorClient/Search/View/LoadingSearchView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct LoadingSearchView: View {
+    let query: String
+    
+    var body: some View {
+        VStack {
+            ActivityIndicator(isAnimating: .constant(true), style: .large)
+            
+            Text("Searching Swift repositories with query: \(query)...")
+                .activityStyle()
+        }
+    }
+}
+
+struct LoadingSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingSearchView(query: "Bow")
+            .previewLayout(.sizeThatFits)
+    }
+}
