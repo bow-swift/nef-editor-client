65802f0e80326d07df50b61aa2e761429e0492c5
Tomás <truizlop@gmail.com>
2020-04-09 11:22:14 +0200

    Update layout for RecipeCatalogView

--- NefEditorClient/Catalog/View/RecipeCatalogView.swift
+++ NefEditorClient/Catalog/View/RecipeCatalogView.swift
@@ -1,10 +1,32 @@
 import SwiftUI
 
 struct RecipeCatalogView: View {
+    let recipes: [Recipe]
+    
     var body: some View {
-        ZStack {
-            Color.gray
-            Text("Catalog")
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Featured recipes")
+                        .largeTitleStyle()
+                    
+                    RecipeGridView(
+                        recipes: self.recipes,
+                        columns: self.columns(for: geometry.size))
+                }.padding()
+            }
         }
+    }
+    
+    private func columns(for size: CGSize) -> Int {
+        let minimumCardWidth: CGFloat = 350
+        return Int(floor(size.width / minimumCardWidth))
+    }
+}
+
+struct RecipeCatalogView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCatalogView(recipes: sampleRecipes)
+            .previewLayout(.fixed(width: 910, height: 1024))
     }
 }
