0d57dd1c1024a82a20e9eeefd1e54d00a2a62907
Tomás <truizlop@gmail.com>
2020-04-15 12:11:38 +0200

    Add computed properties

--- NefEditorClient/Catalog/State/CatalogItem.swift
+++ NefEditorClient/Catalog/State/CatalogItem.swift
@@ -1,4 +1,37 @@
 enum CatalogItem {
     case featured(FeaturedRecipe)
     case regular(Recipe)
+    
+    private func fold<A>(
+        _ ifFeatured: (FeaturedRecipe) -> A,
+        _ ifRegular: (Recipe) -> A
+    ) -> A {
+        switch self {
+        case .featured(let featured): return ifFeatured(featured)
+        case .regular(let recipe): return ifRegular(recipe)
+        }
+    }
+    
+    private func fold<A>(
+        _ ifFeatured: KeyPath<FeaturedRecipe, A>,
+        _ ifRegular: KeyPath<Recipe, A>
+    ) -> A {
+        self.fold({ $0[keyPath: ifFeatured] },
+                  { $0[keyPath: ifRegular] })
+    }
+    
+    var title: String {
+        self.fold(\.recipe.title,
+                  \.title)
+    }
+    
+    var description: String {
+        self.fold(\.recipe.description,
+                  \.description)
+    }
+    
+    var dependencies: [Dependency] {
+        self.fold(\.recipe.dependencies,
+                  \.dependencies)
+    }
 }
