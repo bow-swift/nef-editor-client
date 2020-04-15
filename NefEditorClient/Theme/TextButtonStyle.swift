b3958f35cf06bcb0f33029b2d3000c8ae458d197
Tomás <truizlop@gmail.com>
2020-04-15 13:38:27 +0200

    Add text button style

---
+++ NefEditorClient/Theme/TextButtonStyle.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct TextButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding()
+            .font(Font.system(.body).bold())
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .contentShape(RoundedRectangle(cornerRadius: 8))
+            .background(RoundedRectangle(cornerRadius: 8)
+                .fill(configuration.isPressed
+                    ? Color.nef.opacity(0.7)
+                    : Color.nef))
+            
+    }
+}
