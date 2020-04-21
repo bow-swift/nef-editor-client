1155ff75965837535ec593d122e9ffd564fe3ad4
Tomás <truizlop@gmail.com>
2020-04-21 10:42:20 +0200

    Add copy method

--- NefEditorClient/Catalog/State/Recipe.swift
+++ NefEditorClient/Catalog/State/Recipe.swift
@@ -4,4 +4,15 @@ struct Recipe: Equatable {
     let title: String
     let description: String
     let dependencies: [Dependency]
+    
+    func copy(
+        title: String? = nil,
+        description: String? = nil,
+        dependencies: [Dependency]? = nil
+    ) -> Recipe {
+        Recipe(
+            title: title ?? self.title,
+            description: description ?? self.description,
+            dependencies: dependencies ?? self.dependencies)
+    }
 }
