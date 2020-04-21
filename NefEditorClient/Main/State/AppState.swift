b6596a688e90b16ba992e4240624d594ab40a80a
Tomás <truizlop@gmail.com>
2020-04-21 09:13:10 +0200

    Add copy method

--- NefEditorClient/Main/State/AppState.swift
+++ NefEditorClient/Main/State/AppState.swift
@@ -3,4 +3,17 @@ struct AppState {
     let editState: EditState
     let catalog: Catalog
     let selectedItem: CatalogItem
+    
+    func copy(
+        panelState: PanelState? = nil,
+        editState: EditState? = nil,
+        catalog: Catalog? = nil,
+        selectedItem: CatalogItem? = nil
+    ) -> AppState {
+        AppState(
+            panelState: panelState ?? self.panelState,
+            editState: editState ?? self.editState,
+            catalog: catalog ?? self.catalog,
+            selectedItem: selectedItem ?? self.selectedItem)
+    }
 }
