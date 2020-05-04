bf1b85218142ac8629294a58a5415e42353578a5
Tomás <truizlop@gmail.com>
2020-05-04 16:45:44 +0200

    Add named colors

--- NefEditorClient/Theme/ColorPalette.swift
+++ NefEditorClient/Theme/ColorPalette.swift
@@ -2,14 +2,14 @@ import SwiftUI
 
 extension Color {
     static var nef: Color {
-        Color(red: 57 / 255, green: 6 / 255, blue: 125 / 255)
+        Color("nefColor")
     }
     
-    static var selectedRecipe: Color {
-        Color(red: 246 / 255, green: 244 / 255, blue: 231 / 255)
+    static var card: Color {
+        Color("card")
     }
     
-    static var unselectedRecipe: Color {
-        .white
+    static var shadow: Color {
+        Color("shadow")
     }
 }
