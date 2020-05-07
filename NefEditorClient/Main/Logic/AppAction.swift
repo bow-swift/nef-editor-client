b584a6a00a1e40a8c4c7dc9a0260a71541b391ed
Tomás <truizlop@gmail.com>
2020-05-07 14:25:40 +0200

    Add actions

--- NefEditorClient/Main/Logic/AppAction.swift
+++ NefEditorClient/Main/Logic/AppAction.swift
@@ -5,9 +5,11 @@ enum AppAction: AutoPrism {
     case searchAction(SearchAction)
     case editAction(EditAction)
     case catalogDetailAction(CatalogDetailAction)
+    case creditsAction(CreditsAction)
     case initialLoad
     case dismissModal
     case showAlert
     case dismissAlert
     case showSettings
+    case showCredits
 }
