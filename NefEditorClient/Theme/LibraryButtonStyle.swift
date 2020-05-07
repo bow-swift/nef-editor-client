91769a682763d25fdea24e13d715134a9401885e
Tomás <truizlop@gmail.com>
2020-05-07 13:44:17 +0200

    Add button style

---
+++ NefEditorClient/Theme/LibraryButtonStyle.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct LibraryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(4)
+            .contentShape(RoundedRectangle(cornerRadius: 8))
+            .background(RoundedRectangle(cornerRadius: 8)
+                .fill(configuration.isPressed
+                    ? Color.nef.opacity(0.1)
+                    : Color.clear))
+            
+    }
+}
