875c6d25d33f7c12e5980437003a92b5927474df
Tomás <truizlop@gmail.com>
2020-05-13 17:04:03 +0200

    Extract extension to its own file

--- NefEditorClient/Generation/Logic/GenerationDispatcher.swift
+++ NefEditorClient/Generation/Logic/GenerationDispatcher.swift
@@ -192,19 +192,3 @@ func dismissShareDialog() -> State<AppState, Void> {
         return state.copy(generationState: .finished(item, url, .notSharing))
     }^
 }
-
-extension URLSession {
-    func downloadDataTaskIO(with request: URLRequest) -> IO<Error, Data> {
-        IO.async { callback in
-            self.downloadTask(with: request) { url, response, error in
-                if let url = url, let data = try? Data(contentsOf: url) {
-                    callback(.right(data))
-                } else if let error = error {
-                    callback(.left(error))
-                } else {
-                    callback(.left(GenerationError.dataCorrupted))
-                }
-            }.resume()
-        }^
-    }
-}
